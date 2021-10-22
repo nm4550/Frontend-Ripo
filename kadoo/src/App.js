d5f7997d68dd4ceecf2e166f5a26aa2fbb448465
lhoorie <hoorieh95@gmail.com>
2021-10-22 15:20:40 -0700

    Build: Navbar-Searchbar

--- kadoo/src/App.js
+++ kadoo/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+import Home from "./Pages/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import { ListItemSecondaryAction } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#B8E0D2'
+    },
+    secondary:{
+      main:'#F0D1DE'
+    },
+    error:{
+      main:'#C7AED5'
+    }
+  }
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={theme}>
+    <Router>
+    <div>
+    <Navbar/>
+    <Home/>
     </div>
+    </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ kadoo/src/Components/Navbar/Navbar.js
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import { styled, alpha } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { Button } from '@material-ui/core';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 0.5),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(4% + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Kadoo
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Button href="#text-buttons">Home</Button>
+          <Button href="#text-buttons"
+          >About Us</Button>
+          <Box ml={90}>
+          <Button href="#text-buttons"
+          >Sign Up</Button></Box>
+          <Button href="#text-buttons">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
