040a02ddb0af87959cfcfa59f972f903d8862f75
DenizAhmadi <Ahmadideniz@gmail.com>
2021-11-01 19:03:39 +0330

    Init Frontend

--- kadoo/src/App.js
+++ kadoo/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { createMuiTheme, ThemeProvider } from '@mui/core';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#71c1a5'
+    },
+    secondary:{
+      main:'#d784a6'
+    },
+    error:{
+      main:'#a3a3a3'
+    }
+  }
+})
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+
+      </ThemeProvider>
     </div>
   );
 }
