f544259731d843e5158922752ccae864e925b40e
Elnaz Rezaee <elnazrezaee80@gmail.com>
2021-11-05 20:46:15 +0330

    Responsive/API

--- kadoo/src/Pages/SignIn/SignIn.js
+++ kadoo/src/Pages/SignIn/SignIn.js
@@ -1,107 +1,133 @@
-import React from 'react';
-import Background from '../../Images/SignIn/Background.jpg';
-import { Grid, TextField , Button , InputAdornment } from '@material-ui/core';
-import { EmailRounded , VpnKey} from '@material-ui/icons';
-import axios from 'axios';
-import axiosInstance from '../../axios';
-import { useHistory } from 'react-router';
-import { useState } from 'react';
+import React from 'react'
+import Background from '../../Images/SignIn/Background.jpg'
+import { Grid, TextField, Button, InputAdornment } from '@material-ui/core'
+import { EmailRounded, VpnKey } from '@material-ui/icons'
+import { useHistory } from 'react-router'
+import { useState } from 'react'
 
-function SignIn(){
-    const history = useHistory();
-    const initialFormData = Object.freeze({
-        email:'',
-        password:''
-    });
-    const [formData , updateFormData] = useState(initialFormData);
+function SignIn() {
+  const history = useHistory()
+  const initialFormData = Object.freeze({
+    email: '',
+    password: '',
+  })
+  const [formData, updateFormData] = useState(initialFormData)
 
-    const handleChange = (e) => {
-        updateFormData({
-            ...formData,
-            [e.target.name]: e.target.value.trim(),
-        });
-    };
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    })
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
 
-        axiosInstance
-            .post('token/',{
-                email: formData.email,
-                password: formData.password,
-            })
-            .then((res) => {
-                localStorage.setItem('access_token',res.data.access);
-                localStorage.setItem('refresh_token',res.data.refresh);
-                axiosInstance.defaults.headers['Authorixation'] = 
-                    'JWT ' + localStorage.getItem('access_token');
-                    history.push('/');
-            });
-    };
-    return(
-        <div BackgroundColor="secondary">
-            <Grid container style={{minHeight: '100vh'}}>
-                <Grid item xs={12} sm={6}>
-                    <img src={Background} style={{width: '100%' , height: '100%' , objectFit:'cover'}} alt="Background"/>
-                </Grid>
-                <Grid 
-                container 
-                item 
-                xs={12} 
-                sm={6}
-                alignItems="center"
-                direction="column"
-                justify="space-between" 
-                style={{padding: 10}}>
-                    <div/>
-                    <div 
-                    style={{
-                        display:"flex" , 
-                        flexDirection:"column" ,
-                        maxWidth: 400,
-                        minWidth: 300}}>
-                        
-                        <TextField 
-                        variant="standard"
-                        name="email"
-                        label="Email" 
-                        margin="normal"
-                        InputProps={{startAdornment:<InputAdornment position="start"> <EmailRounded/> </InputAdornment>}} 
-                        onChange={handleChange} /> 
-                        <TextField 
-                        variant="standard"
-                        type="password"
-                        name="password"
-                        label="Password" 
-                        margin="normal"
-                        InputProps={{startAdornment:<InputAdornment position="start"> <VpnKey/> </InputAdornment>}}
-                        onChange={handleChange} />
-                        <div style={{height: 20}}/> 
-                        <Button color="secondary" variant="contained" onClick={handleSubmit}>
-                            Sign In
-                        </Button>
-                        <div style={{height: 20}}/> 
-                        <Button color="secondary" variant="outlined">
-                            Sign Up
-                        </Button>
-                    </div>
-                    <Grid container justifyContent="center" spacing={2}>
-                        <Grid item>
-                            <Button variant="outlined" color="secondary">
-                                Go to community page
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="outlined" color="secondary">
-                                Forgot password
-                            </Button>   
-                        </Grid>
-                    </Grid>
-                </Grid>
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password,
+      }),
+    }
+    fetch('http://127.0.0.1:8000/api/user/token/', requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('access_token', data.access)
+        localStorage.setItem('refresh_token', data.refresh)
+      })
+  }
+  return (
+    <div BackgroundColor='secondary'>
+      <Grid container style={{ minHeight: '100vh' }}>
+        <Grid item xs={12} sm={6}>
+          <img
+            src={Background}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            alt='Background'
+          />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          alignItems='center'
+          direction='column'
+          justify='space-between'
+          style={{ padding: 10 }}
+        >
+          <div />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              maxWidth: 400,
+              minWidth: 300,
+            }}
+          >
+            <TextField
+              variant='standard'
+              name='email'
+              label='Email'
+              margin='normal'
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <EmailRounded />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <TextField
+              variant='standard'
+              type='password'
+              name='password'
+              label='Password'
+              margin='normal'
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <VpnKey />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <div style={{ height: 20 }} />
+            <Button
+              color='secondary'
+              variant='contained'
+              onClick={handleSubmit}
+            >
+              Sign In
+            </Button>
+            <div style={{ height: 20 }} />
+            <Button color='secondary' variant='outlined'>
+              Sign Up
+            </Button>
+          </div>
+          <Grid container justifyContent='center' spacing={2}>
+            <Grid item>
+              <Button variant='outlined' color='secondary'>
+                Go to community page
+              </Button>
             </Grid>
-        </div>
-    );
+            <Grid item>
+              <Button variant='outlined' color='secondary'>
+                Forgot password
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
-export default SignIn;
+export default SignIn
--- kadoo/src/Pages/SignUp/SignUp.js
+++ kadoo/src/Pages/SignUp/SignUp.js
@@ -1,141 +1,194 @@
-import React , {useState} from 'react';
-import axiosInstance from '../../axios';
-import Background from '../../Images/SignIn/Background.jpg';
-import { Grid, TextField , Button , InputAdornment } from '@material-ui/core';
-import { AccountCircle , VpnKey , EmailSharp , Create } from '@material-ui/icons';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react'
+import Background from '../../Images/SignIn/Background.jpg'
+import { Grid, TextField, Button, InputAdornment } from '@material-ui/core'
+import { AccountCircle, VpnKey, EmailSharp, Create } from '@material-ui/icons'
+import { useHistory } from 'react-router-dom'
 
-function SignUp(){
-    const history = useHistory();
-    const initialFormData = Object.freeze({
-        name:'',
-        lastName:'',
-        userName:'',
-        email:'',
-        password:''
-    });
-    const [formData , updateFormData] = useState(initialFormData);
+function SignUp() {
+  const history = useHistory()
+  const initialFormData = Object.freeze({
+    name: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    password: '',
+  })
+  const [formData, updateFormData] = useState(initialFormData)
 
-    const handleChange = (e) => {
-        updateFormData({
-            ...formData,
-            [e.target.name]: e.target.value.trim(),
-        });
-        console.log(formData);
-    };
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    })
+    console.log(formData)
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
-        console.log("email:" + formData.email);
-        console.log("username:" + formData.userName);
-        console.log("Name:" + formData.name);
-        console.log("Last name:" + formData.lastName);
-        console.log("password:" + formData.password);
-        console.log( JSON.stringify({
-            "email": formData.email,
-            "user_name": formData.userName,
-            "first_name": formData.name,
-            "last_name": formData.lastName,
-            "password": formData.password,
-        }))
-        axiosInstance
-            .post(`user/register/`, JSON.stringify({
-                "email": formData.email,
-                "user_name": formData.userName,
-                "first_name": formData.name,
-                "last_name": formData.lastName,
-                "password": formData.password,
-            }))
-            .then((res) => {
-                //history.push('/SignIn');
-                console.log(res);
-                //console.log(res.data);
-            });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    console.log('email:' + formData.email)
+    console.log('username:' + formData.userName)
+    console.log('Name:' + formData.name)
+    console.log('Last name:' + formData.lastName)
+    console.log('password:' + formData.password)
+    console.log(
+      JSON.stringify({
+        email: formData.email,
+        user_name: formData.userName,
+        first_name: formData.name,
+        last_name: formData.lastName,
+        password: formData.password,
+      })
+    )
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: formData.email,
+        user_name: formData.userName,
+        first_name: formData.name,
+        last_name: formData.lastName,
+        password: formData.password,
+      }),
+    }
+    fetch('http://127.0.0.1:8000/api/user/register/', requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }
 
-    return(
-        <div BackgroundColor="secondary">
-            <Grid container style={{minHeight: '100vh'}}>
-                <Grid item xs={12} sm={6}>
-                    <img src={Background} style={{width: '100%' , height: '100%' , objectFit:'cover'}} alt="Background"/>
-                </Grid>
-                <Grid 
-                container 
-                item 
-                xs={12} 
-                sm={6}
-                alignItems="center"
-                direction="column"
-                justify="space-between" 
-                style={{padding: 10}}>
-                    <div/>
-                    <div 
-                    style={{
-                        display:"flex" , 
-                        flexDirection:"column" ,
-                        maxWidth: 400,
-                        minWidth: 300}}>
-                        <TextField 
-                        id="name"
-                        name="name"
-                        variant="standard"
-                        label="Name" 
-                        margin="normal"
-                        required
-                        InputProps={{startAdornment:<InputAdornment position="start"> <Create/> </InputAdornment>}}
-                        onChange={handleChange} /> 
-                        <TextField 
-                        id="lastName"
-                        name="lastName"
-                        variant="standard"
-                        label="Last name" 
-                        margin="normal"
-                        required
-                        InputProps={{startAdornment:<InputAdornment position="start"> <AccountCircle/> </InputAdornment>}}
-                        onChange={handleChange} /> 
-                        <TextField 
-                        id="userName"
-                        name="userName"
-                        variant="standard"
-                        label="Username" 
-                        margin="normal"
-                        required
-                        InputProps={{startAdornment:<InputAdornment position="start"> <AccountCircle/> </InputAdornment>}}
-                        onChange={handleChange} /> 
-                        <TextField 
-                        id="email"
-                        name="email"
-                        variant="standard"
-                        label="Email" 
-                        margin="normal"
-                        required
-                        InputProps={{startAdornment:<InputAdornment position="start"> <EmailSharp/> </InputAdornment>}}
-                        onChange={handleChange} /> 
-                        <TextField 
-                        id="password"
-                        name="password"
-                        variant="standard"
-                        type="password"
-                        label="Password" 
-                        margin="normal"
-                        required
-                        InputProps={{startAdornment:<InputAdornment position="start"> <VpnKey/> </InputAdornment>}}
-                        onChange={handleChange} />
-                        <div style={{height: 20}}/> 
-                        <Button color="secondary" variant="contained" onClick={handleSubmit}>
-                            Sign Up
-                        </Button>
-                        <div style={{height: 20}}/> 
-                        <Button color="secondary" variant="outlined">
-                            Have an account ?
-                        </Button>
-                    </div>
-                    <div/>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  return (
+    <div BackgroundColor='secondary'>
+      <Grid container style={{ minHeight: '100vh' }}>
+        <Grid item xs={12} sm={6}>
+          <img
+            src={Background}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            alt='Background'
+          />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          alignItems='center'
+          direction='column'
+          justify='space-between'
+          style={{ padding: 10 }}
+        >
+          <div />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              maxWidth: 400,
+              minWidth: 300,
+            }}
+          >
+            <TextField
+              id='name'
+              name='name'
+              variant='standard'
+              label='Name'
+              margin='normal'
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <Create />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <TextField
+              id='lastName'
+              name='lastName'
+              variant='standard'
+              label='Last name'
+              margin='normal'
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <AccountCircle />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <TextField
+              id='userName'
+              name='userName'
+              variant='standard'
+              label='Username'
+              margin='normal'
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <AccountCircle />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <TextField
+              id='email'
+              name='email'
+              variant='standard'
+              label='Email'
+              margin='normal'
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <EmailSharp />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <TextField
+              id='password'
+              name='password'
+              variant='standard'
+              type='password'
+              label='Password'
+              margin='normal'
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <VpnKey />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
+            <div style={{ height: 20 }} />
+            <Button
+              color='secondary'
+              variant='contained'
+              onClick={handleSubmit}
+            >
+              Sign Up
+            </Button>
+            <div style={{ height: 20 }} />
+            <Button color='secondary' variant='outlined'>
+              Have an account ?
+            </Button>
+          </div>
+          <div />
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
-export default SignUp;
+export default SignUp
