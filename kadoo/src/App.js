cc9dfbb346451cb82a6fdc23e29646926b042140
DenizAhmadi <ahmadideniz@gmail.com>
2021-11-12 23:26:48 +0330

    bug: import fix

--- kadoo/src/App.js
+++ kadoo/src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, BrowserRouter ,Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Pages } from '@mui/icons-material';
-import './App.css';
 import ProductsPage from './Pages/ProductPlantsPage/ProductsPage';
 
 function App() {
