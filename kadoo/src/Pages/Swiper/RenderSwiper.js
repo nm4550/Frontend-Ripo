6952e772102369953bcf897e43a497fb03461901
Elnaz Rezaee <elnazrezaee80@gmail.com>
2021-12-13 20:17:38 +0330

    fix:AboutUs/ContactUs

--- kadoo/src/Pages/Swiper/RenderSwiper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "swiper/css/bundle";
-import "./swiper.css";
-import Swiper from './Swiper.js';
-
-ReactDOM.render(<Swiper/>, document.getElementById("app"));
