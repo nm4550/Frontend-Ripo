fac01eda18e97ec60e32eecd358701fa4885bd3a
Navid.MSZD <s.navidm1380@gmail.com>
2022-01-01 11:40:41 +0330

    feat: reminder functionality

--- kadoo/src/Components/Reminder.js
+++ kadoo/src/Components/Reminder.js
@@ -25,50 +25,98 @@ import TimePicker from '@mui/lab/TimePicker'
 
 export default function Reminder() {
   const [value, setValue] = React.useState(null)
-  const [weekDays, setWeekDays] = React.useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ])
+  const [num, setNum] = React.useState(1)
+  const [satEnable, setSatEnable] = React.useState(false)
+  const [sunEnable, setSunEnable] = React.useState(false)
+  const [monEnable, setMonEnable] = React.useState(false)
+  const [tueEnable, setTueEnable] = React.useState(false)
+  const [wedEnable, setWedEnable] = React.useState(false)
+  const [thuEnable, setThuEnable] = React.useState(false)
+  const [friEnable, setFriEnable] = React.useState(false)
+
+  const [timeEnable1, setTimeEnable1] = React.useState(true)
+  const [timeEnable2, setTimeEnable2] = React.useState(false)
+  const [timeEnable3, setTimeEnable3] = React.useState(false)
+  const [timeEnable4, setTimeEnable4] = React.useState(false)
+
+  const [time1, setTime1] = React.useState(null)
+  const [time2, setTime2] = React.useState(null)
+  const [time3, setTime3] = React.useState(null)
+  const [time4, setTime4] = React.useState(null)
+
+  const [timeFormat1, setTimeFormat1] = React.useState('')
+  const [timeFormat2, setTimeFormat2] = React.useState('')
+  const [timeFormat3, setTimeFormat3] = React.useState('')
+  const [timeFormat4, setTimeFormat4] = React.useState('')
 
   function handleClick(index) {
-    if (weekDays[index]) {
-      setWeekDays((pre) => [
-        ...pre.slice(0, index),
-        false,
-        ...pre.slice(index + 1),
-      ])
-    } else {
-      setWeekDays((pre) => [
-        ...pre.slice(0, index),
-        true,
-        ...pre.slice(index + 1),
-      ])
+    switch (index) {
+      case 0:
+        setSatEnable(satEnable ? false : true)
+        break
+      case 1:
+        setSunEnable(sunEnable ? false : true)
+        break
+      case 2:
+        setMonEnable(monEnable ? false : true)
+        break
+      case 3:
+        setTueEnable(tueEnable ? false : true)
+        break
+      case 4:
+        setWedEnable(wedEnable ? false : true)
+        break
+      case 5:
+        setThuEnable(thuEnable ? false : true)
+        break
+      case 6:
+        setFriEnable(friEnable ? false : true)
+        break
     }
     console.log('You clicked the Chip.')
   }
-  // render() {
-  //             var increaseBought=()=>{
-  //           var nob=this.state.numberOfBuy
-  //               this.setState({
-  //                   numberOfBuy:nob+1,
-  //                   totalPrice:(nob+1)*this.state.product.price
-  //           })
-  //       }
-  //       var decreaseBought=()=>{
-  //           var nob=this.state.numberOfBuy
-  //           if (nob > 0){
-  //               this.setState({
-  //                   numberOfBuy:nob-1,
-  //                   totalPrice:(nob-1)*this.state.product.price
-  //           })
-  //           }
-  //       }
-  // }
+  const updateText = () => {
+    switch (num) {
+      case 1:
+        setTimeEnable1(true)
+        setTimeEnable2(false)
+        setTimeEnable3(false)
+        setTimeEnable4(false)
+        break
+      case 2:
+        setTimeEnable1(true)
+        setTimeEnable2(true)
+        setTimeEnable3(false)
+        setTimeEnable4(false)
+        break
+      case 3:
+        setTimeEnable1(true)
+        setTimeEnable2(true)
+        setTimeEnable3(true)
+        setTimeEnable4(false)
+        break
+      case 4:
+        setTimeEnable1(true)
+        setTimeEnable2(true)
+        setTimeEnable3(true)
+        setTimeEnable4(true)
+        break
+    }
+  }
+  useEffect(() => {
+    updateText()
+  }, [num])
+  var increaseBought = () => {
+    if (num < 4) {
+      setNum(num + 1)
+    }
+  }
+  var decreaseBought = () => {
+    if (num > 1) {
+      setNum(num - 1)
+    }
+  }
+
   return (
     <Grid>
       <Grid
@@ -97,44 +145,50 @@ export default function Reminder() {
             <Chip
               label='SAT'
               onClick={() => handleClick(0)}
-              color={weekDays[0] ? 'primary' : 'default'}
+              color={satEnable ? 'primary' : 'default'}
             />
             <Chip
               label='SUN'
-              onClick={handleClick(1)}
-              color={weekDays[1] ? 'primary' : 'default'}
+              onClick={() => handleClick(1)}
+              color={sunEnable ? 'primary' : 'default'}
             />
             <Chip
               label='MON'
-              onClick={handleClick(2)}
-              color={weekDays[2] ? 'primary' : 'default'}
+              onClick={() => handleClick(2)}
+              color={monEnable ? 'primary' : 'default'}
             />
             <Chip
               label='TUE'
-              onClick={handleClick(3)}
-              color={weekDays[3] ? 'primary' : 'default'}
+              onClick={() => handleClick(3)}
+              color={tueEnable ? 'primary' : 'default'}
             />
             <Chip
               label='WED'
-              onClick={handleClick(4)}
-              color={weekDays[4] ? 'primary' : 'default'}
+              onClick={() => handleClick(4)}
+              color={wedEnable ? 'primary' : 'default'}
             />
             <Chip
               label='THU'
-              onClick={handleClick(5)}
-              color={weekDays[5] ? 'primary' : 'default'}
+              onClick={() => handleClick(5)}
+              color={thuEnable ? 'primary' : 'default'}
             />
             <Chip
               label='FRI'
-              onClick={handleClick(6)}
-              color={weekDays[6] ? 'primary' : 'default'}
+              onClick={() => handleClick(6)}
+              color={friEnable ? 'primary' : 'default'}
             />
           </Stack>
         </Grid>
         <Grid container justifyContent='center' alignItems='center'>
           <Divider sx={{ m: 2 }} style={{ width: '50%' }} />
         </Grid>
-        <Grid justifyContent='center' alignItems='center'>
+        <Grid
+          container
+          direction='row'
+          alignItems='center'
+          justifyContent='center'
+        ></Grid>
+        <Grid item justifyContent='center' alignItems='center'>
           <Typography
             variant='h5'
             gutterBottom
@@ -145,25 +199,29 @@ export default function Reminder() {
             How many times day?
           </Typography>
         </Grid>
-        <Grid justifyContent='center' alignItems='center'>
+        <Grid
+          item
+          container
+          alignItems='center'
+          justifyContent='center'
+          direction='row'
+        >
           <IconButton
             size='large'
             aria-label='show 4 new mails'
             color='inherit'
             sx={{ color: 'error.main' }}
-            // onClick={decreaseBought}
+            onClick={decreaseBought}
           >
             <RemoveIcon />
           </IconButton>
-          {/* <div className="ProductPageCounterNum">
-              {this.state.numberOfBuy}
-          </div> */}
+          <div className='ProductPageCounterNum'>{num}</div>
           <IconButton
             size='large'
             aria-label='show 4 new mails'
             color='inherit'
             sx={{ color: 'success.main' }}
-            // onClick={increaseBought}
+            onClick={increaseBought}
           >
             <AddIcon />
           </IconButton>
@@ -184,22 +242,58 @@ export default function Reminder() {
         </Grid>
         <Grid container justifyContent='center' alignItems='center'>
           <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <TimePicker
-              label='Basic example'
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue)
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-            <TimePicker
-              label='Basic example'
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue)
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
+            {timeEnable1 && (
+              <Box sx={{ p: 1 }}>
+                <TimePicker
+                  sx={{ m: 1 }}
+                  label='First time'
+                  value={time1}
+                  onChange={(newValue) => {
+                    setTime1(newValue)
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Box>
+            )}
+            {timeEnable2 && (
+              <Box sx={{ p: 1 }}>
+                <TimePicker
+                  sx={{ m: 1 }}
+                  label='Second time'
+                  value={time2}
+                  onChange={(newValue) => {
+                    setTime2(newValue)
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Box>
+            )}
+            {timeEnable3 && (
+              <Box sx={{ p: 1 }}>
+                <TimePicker
+                  sx={{ m: 1 }}
+                  label='Third time'
+                  value={time3}
+                  onChange={(newValue) => {
+                    setTime3(newValue)
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Box>
+            )}
+            {timeEnable4 && (
+              <Box sx={{ p: 1 }}>
+                <TimePicker
+                  sx={{ m: 1 }}
+                  label='Fourth time'
+                  value={time4}
+                  onChange={(newValue) => {
+                    setTime4(newValue)
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Box>
+            )}
           </LocalizationProvider>
         </Grid>
         <Grid
